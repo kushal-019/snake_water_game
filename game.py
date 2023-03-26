608f8bac6693ea0ee3415b1812dfa3d8159c5364
kushal goel <kgoel8451@gmail.com>
2023-03-26 15:51:32 +0530

    fully developed game and checked with functions

--- game.py
+++ game.py
@@ -11,4 +11,16 @@ def swg_game():
         print("you wins")    
     elif me=="s" and comp=="g":
         print('comp wins')   
-    
+    elif me=="g" and comp=="w":
+        print("comp wins")
+    elif me=="g" and comp=="s":
+        print('you wins')
+    elif me=="w" and comp=="g":
+        print("you wins")
+    elif me=="w" and comp=="s":
+        print('comp wins')
+    elif me==comp:
+        print("tie")
+    else:
+        print("invalid input")
+    return
